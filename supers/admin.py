4fd1d31a0b40beb79f8ed11d3fae3a2f2acbe3a9
wmalott23 <101433624+wmalott23@users.noreply.github.com>
2022-04-04 14:22:30 -0700

    Added new power model + added new power model to admin + migrated new power model + removed primary and secondary powers, and added powers variable with new power model

--- supers/admin.py
+++ supers/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Power
 
 # Register your models here.
+
+admin.site.register(Power)
--- supers/models.py
+++ supers/models.py
@@ -2,11 +2,13 @@ from django.db import models
 from super_types.models import SuperType
 
 # Create your models here.
+class Power(models.Model):
+    name = models.CharField(max_length=40)
 
 class Super(models.Model):
     name = models.CharField(max_length=40)
     alter_ego = models.CharField(max_length=40)
-    primary_ability = models.CharField(max_length=40)
-    secondary_ability = models.CharField(max_length=40)
+    powers = models.ManyToManyField(Power)
     catchphrase = models.CharField(max_length=255)
     super_type = models.ForeignKey(SuperType, on_delete=models.CASCADE)
+
