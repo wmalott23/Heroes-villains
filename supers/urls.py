4a6bd1cd62c8ccf6566575e14dc0f326b0125c19
wmalott23 <101433624+wmalott23@users.noreply.github.com>
2022-04-04 14:55:55 -0700

    Saved a file

--- supers/urls.py
+++ supers/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.supers_list),
     path('<int:pk>/', views.supers_detail),
-    path('<int:pk>/<int:power>/', views.supers_power)
+    path('<int:pk>/<int:powers>/', views.supers_power)
 ]
